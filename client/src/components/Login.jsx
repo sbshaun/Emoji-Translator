dc4916b3b20d2af7ed630b4323747b896156098a
sbshaun <sbshaun7@gmail.com>
2023-02-27 18:16:41 -0800

    update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,6 @@ import styles from '../styles/Login.module.scss';
 
 const Login = ({ setUser }) => {
   const handleSignIn = async () => {
-    console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
     const userCredential = await googleSignIn();
     setUser(userCredential.user);
   };
