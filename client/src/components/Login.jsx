e90dbfae18008af6c636989637db6cda481bfa79
sbshaun <sbshaun7@gmail.com>
2023-03-01 22:43:11 -0800

    centered login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -25,17 +25,19 @@ const Login = ({ setUser }) => {
   };
 
   return (
-    <div className={styles.googleBtn} onClick={handleSignIn}>
-      <div className={styles.googleIconWrapper}>
-        <img
-          className={styles.googleIcon}
-          alt="google icon"
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-        />
+    <div className={styles.loginContainer}>
+      <div className={styles.googleBtn} onClick={handleSignIn}>
+        <div className={styles.googleIconWrapper}>
+          <img
+            className={styles.googleIcon}
+            alt="google icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+          />
+        </div>
+        <p className={styles.btnText}>
+          <b>Sign in with Google</b>
+        </p>
       </div>
-      <p className={styles.btnText}>
-        <b>Sign in with Google</b>
-      </p>
     </div>
   );
 };
